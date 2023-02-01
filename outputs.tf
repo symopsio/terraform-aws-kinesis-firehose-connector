819643e35f895e92e8af8d26c4babc4e2a2c98b5
Leslie Lam <leslie@symops.io>
2023-02-01 12:01:46 -0500

    Apply tags to generated S3 bucket and IAM policies (#1)

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,10 @@ data "aws_region" "current" {}
 
 output "firehose_role_arn" {
   description = "The ARN of the created IAM role to be used by the Firehose"
-  value = aws_iam_role.firehose_role.arn
+  value       = aws_iam_role.firehose_role.arn
 }
 
 output "firehose_bucket_arn" {
   description = "The ARN of the created S3 bucket to be used by the Firehose"
-  value = module.s3_bucket.bucket_arn
+  value       = module.s3_bucket.bucket_arn
 }
